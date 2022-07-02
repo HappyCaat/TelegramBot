4d398e8c780ff910e56d1e7417bd32772b11280f
Igor Penkin <ipenkin86@gmail.com>
2022-07-02 11:43:57 +0300

    02.07.22 Add buttons

--- src/main/java/BotCore.java
+++ src/main/java/BotCore.java
@@ -1,7 +1,13 @@
 import org.telegram.telegrambots.bots.TelegramLongPollingBot;
 import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
 import org.telegram.telegrambots.meta.api.objects.Update;
+import org.telegram.telegrambots.meta.api.objects.replykeyboard.ReplyKeyboardMarkup;
+import org.telegram.telegrambots.meta.api.objects.replykeyboard.buttons.KeyboardButton;
+import org.telegram.telegrambots.meta.api.objects.replykeyboard.buttons.KeyboardRow;
 import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class BotCore extends TelegramLongPollingBot {
     private static final String START = "start";
@@ -10,15 +16,38 @@ public class BotCore extends TelegramLongPollingBot {
     @Override
     public void onUpdateReceived(Update update) {
         // TODO writing logic...
+        SendMessage sendMessage = new SendMessage();
 
         if (update.hasMessage() && update.getMessage().hasText()) {
             switch (update.getMessage().getText()) {
                 case START -> {
-                    SendMessage sendMessage = new SendMessage();
+
                     sendMessage.setChatId(String.valueOf(update.getMessage().getChatId()));
                     sendMessage.setText("Hello World!");
+
+                    //initKeyboard
+                    ReplyKeyboardMarkup replyKeyboardMarkup = new ReplyKeyboardMarkup();
+                    replyKeyboardMarkup.setResizeKeyboard(true);
+                    List<KeyboardRow> keyboardRowList = new ArrayList<>();
+                    KeyboardRow keyboardRow1 = new KeyboardRow();
+                    KeyboardRow keyboardRow2 = new KeyboardRow();
+                    KeyboardButton keyboardButton1 = new KeyboardButton();
+                    KeyboardButton keyboardButton2 = new KeyboardButton();
+                    KeyboardButton keyboardButton3 = new KeyboardButton();
+                    keyboardButton1.setText("Что умеет этот бот");
+                    keyboardButton2.setText("Справка Java");
+                    keyboardButton3.setText("Поиск");
+                    keyboardRow1.add(keyboardButton1);
+                    keyboardRow2.add(keyboardButton2);
+                    keyboardRow1.add(keyboardButton3);
+                    keyboardRowList.add(keyboardRow1);
+                    keyboardRowList.add(keyboardRow2);
+                    replyKeyboardMarkup.setKeyboard(keyboardRowList);
+                    sendMessage.setReplyMarkup(replyKeyboardMarkup);
+
                     try {
                         execute(sendMessage);
+
                     } catch (TelegramApiException e) {
                         e.printStackTrace();
                     }
@@ -26,6 +55,7 @@ public class BotCore extends TelegramLongPollingBot {
             }
         }
     }
+
 
     @Override
     public String getBotUsername() {
