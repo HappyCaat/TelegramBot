1f50abd7dde2e015cf92e6ba70cadbaca3423926
Igor Penkin <ipenkin86@gmail.com>
2022-07-02 13:33:21 +0300

    02.07.22 add reaction on buttons+constants class

--- src/main/java/BotCore.java
+++ src/main/java/BotCore.java
@@ -1,5 +1,6 @@
 import org.telegram.telegrambots.bots.TelegramLongPollingBot;
 import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
+import org.telegram.telegrambots.meta.api.objects.Message;
 import org.telegram.telegrambots.meta.api.objects.Update;
 import org.telegram.telegrambots.meta.api.objects.replykeyboard.ReplyKeyboardMarkup;
 import org.telegram.telegrambots.meta.api.objects.replykeyboard.buttons.KeyboardButton;
@@ -10,40 +11,54 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class BotCore extends TelegramLongPollingBot {
-    private static final String START = "start";
-
 
     @Override
     public void onUpdateReceived(Update update) {
         // TODO writing logic...
-        SendMessage sendMessage = new SendMessage();
+
+        Message message = update.getMessage();
 
         if (update.hasMessage() && update.getMessage().hasText()) {
-            switch (update.getMessage().getText()) {
-                case START -> {
+            SendMessage sendMessage = new SendMessage();
+
+            //initKeyboard
+            ReplyKeyboardMarkup replyKeyboardMarkup = new ReplyKeyboardMarkup();
+            replyKeyboardMarkup.setResizeKeyboard(true);
+            List<KeyboardRow> keyboardRowList = new ArrayList<>();
+            KeyboardRow keyboardRow1 = new KeyboardRow();
+            KeyboardRow keyboardRow2 = new KeyboardRow();
+            KeyboardButton keyboardButton1 = new KeyboardButton();
+            KeyboardButton keyboardButton2 = new KeyboardButton();
+            KeyboardButton keyboardButton3 = new KeyboardButton();
+            keyboardButton1.setText("Что умеет этот бот");
+            keyboardButton2.setText("Справка Java");
+            keyboardButton3.setText("Поиск");
+            keyboardRow1.add(keyboardButton1);
+            keyboardRow2.add(keyboardButton2);
+            keyboardRow1.add(keyboardButton3);
+            keyboardRowList.add(keyboardRow1);
+            keyboardRowList.add(keyboardRow2);
+            replyKeyboardMarkup.setKeyboard(keyboardRowList);
+            sendMessage.setReplyMarkup(replyKeyboardMarkup);
+
+
+            switch (message.getText()) {
+                case Constants.START -> {
 
                     sendMessage.setChatId(String.valueOf(update.getMessage().getChatId()));
                     sendMessage.setText("Hello World!");
 
-                    //initKeyboard
-                    ReplyKeyboardMarkup replyKeyboardMarkup = new ReplyKeyboardMarkup();
-                    replyKeyboardMarkup.setResizeKeyboard(true);
-                    List<KeyboardRow> keyboardRowList = new ArrayList<>();
-                    KeyboardRow keyboardRow1 = new KeyboardRow();
-                    KeyboardRow keyboardRow2 = new KeyboardRow();
-                    KeyboardButton keyboardButton1 = new KeyboardButton();
-                    KeyboardButton keyboardButton2 = new KeyboardButton();
-                    KeyboardButton keyboardButton3 = new KeyboardButton();
-                    keyboardButton1.setText("Что умеет этот бот");
-                    keyboardButton2.setText("Справка Java");
-                    keyboardButton3.setText("Поиск");
-                    keyboardRow1.add(keyboardButton1);
-                    keyboardRow2.add(keyboardButton2);
-                    keyboardRow1.add(keyboardButton3);
-                    keyboardRowList.add(keyboardRow1);
-                    keyboardRowList.add(keyboardRow2);
-                    replyKeyboardMarkup.setKeyboard(keyboardRowList);
-                    sendMessage.setReplyMarkup(replyKeyboardMarkup);
+                    try {
+                        execute(sendMessage);
+
+                    } catch (TelegramApiException e) {
+                        e.printStackTrace();
+                    }
+                }
+                case Constants.TASK -> {
+
+                    sendMessage.setChatId(String.valueOf(update.getMessage().getChatId()));
+                    sendMessage.setText("Этот бот собрал информацию по основным разделам документации Java. И с радостью готов ею поделиться с вами");
 
                     try {
                         execute(sendMessage);
@@ -55,7 +70,6 @@ public class BotCore extends TelegramLongPollingBot {
             }
         }
     }
-
 
     @Override
     public String getBotUsername() {
---
+++ src/main/java/Constants.java
@@ -0,0 +1,5 @@
+public class Constants {
+
+    public static final String START = "start";
+    public static final String TASK = "Что умеет этот бот";
+}
